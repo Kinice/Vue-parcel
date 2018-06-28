8caea5b7fc3ea6c780cc0c41d4885f6b04bc3c17
Kinice <szp93@126.com>
2018-06-28 15:40:25 +0800

    Init repo.

---
+++ index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from './src/app.vue'
+console.log(Vue)
+
+import Hello from './src/pages/hello.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'hash',
+  linkActiveClass: 'active',
+  routes: [{
+    path: '/',
+    name: 'Hello',
+    component: Hello
+  }]
+})
+
+new Vue({
+  el: '#app',
+  router,
+  render: (h) => h(App)
+});
---
+++ src/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    你好啊
+  </div>
+</template>
+
+<script>
+import app from './app.js'
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  overflow: scroll;
+  -webkit-overflow-scrolling: touch;
+}
+</style>
